bc0f691197efc8ab49e44ceba879b41355bbf1d4
steve476 <64228013+steve476@users.noreply.github.com>
2026-06-16 09:44:21 +0300

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,3 +0,0 @@
-alter table listings add column if not exists job_mode text;
-alter table listings add column if not exists job_type text;
-alter table listings add column if not exists skills_required text;
